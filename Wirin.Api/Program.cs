933fc7b8f0ee46efe54f3c483dd1b49ae5f74d66
NROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2025-06-23 11:26:57 -0300

    Update Program.cs

--- Wirin.Api/Program.cs
+++ Wirin.Api/Program.cs
@@ -181,7 +181,7 @@ builder.Logging.SetMinimumLevel(LogLevel.Trace);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
